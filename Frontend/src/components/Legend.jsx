7795393a3331127a0914429728e9b66eb9b97915
majikyouser <sahiagb@gmail.com>
2023-09-29 16:57:54 -0400

    fisnfis

--- Frontend/src/components/Legend.jsx
+++ Frontend/src/components/Legend.jsx
@@ -2,8 +2,8 @@
 const Legend = () => {
     return (
         <div className="legend">
-            <div style={{ "--color": '#000000' }}>Higher Priority Areas</div>
-            <div style={{ "--color": '#f8f8f8' }}>Lower Priority Areas</div>
+            <div style={{ "--color": '#000000' }}>Higher Energy Areas</div>
+            <div style={{ "--color": '#f8f8f8' }}>Lower Energy Areas</div>
             <div>Blue Markers are for notable cities that have an extraordinarily large wind speed compared to population</div>
         </div>
     );
